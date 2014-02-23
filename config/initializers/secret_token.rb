4c33249d683bbdc76b79578c19cd1a8217a85e84
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2014-02-23 14:53:10 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CkeditorTest::Application.config.secret_token = '5900bc69484b77189e7a81df652469e0a1a2e940bdea458477096f4e59ce7edbe08a46ca5721fc30a13487aaa506c07ed996b07c2a9b4983f45f8c64d805ecca'
